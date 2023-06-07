cfb62b6e4faacea330363d49c54bfc03cff0fcc1
MaxDeb1 <deb1maxime@gmail.com>
2023-06-07 22:13:59 +0200

    commit

---
+++ client/src/pages/Books.tsx
@@ -0,0 +1,66 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import BooksData from "../types/Books";
+
+const Books = () => {
+  const [books, setBooks] = useState<BooksData[]>([]);
+
+  useEffect(() => {
+    const fetchAllBooks = async () => {
+      try {
+        const res = await axios.get("http://localhost:8080/books");
+        setBooks(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchAllBooks();
+  }, []);
+
+  console.log(books);
+
+  const handleDelete = async (id: number) => {
+    try {
+      await axios.delete(`http://localhost:8080/books/${id}`);
+      window.location.reload();
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <div>
+      <h1>GoodReader - Book Shop</h1>
+      <div className="books">
+        {books.map((book) => (
+          <div key={book.id} className="book">
+            <img src={book.cover} alt="" />
+            <h2>{book.title}</h2>
+            <p>{book.desc}</p>
+            <span>${book.price}</span>
+            <button className="delete" onClick={() => handleDelete(book.id)}>
+              Delete
+            </button>
+            <button className="update">
+              <Link
+                to={`/update/${book.id}`}
+                style={{ color: "inherit", textDecoration: "none" }}
+              >
+                Update
+              </Link>
+            </button>
+          </div>
+        ))}
+      </div>
+
+      <button className="addHome">
+        <Link to="/add" style={{ color: "inherit", textDecoration: "none" }}>
+          Add new book
+        </Link>
+      </button>
+    </div>
+  );
+};
+
+export default Books;
---
+++ client/src/types/Books.ts
@@ -0,0 +1,7 @@
+export default interface BooksData {
+  id: number;
+  title: string;
+  desc: string;
+  price: number;
+  cover?: string;
+}
